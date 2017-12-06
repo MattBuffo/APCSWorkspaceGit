fd2dd5ef2cd9a56028016c429fde47d795398426
MattBuffo <mattthel33t@gmail.com>
2017-12-06 14:23:07 -0500

    mechanic class work

--- InheritanceProject/src/Mechanic.java
+++ InheritanceProject/src/Mechanic.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Mechanic {
-	private static ArrayList<Vehicle> Garage = new ArrayList<Vehicle>();
+
 	public static void main(String[] args) {
+		Scanner io = new Scanner(System.in);
+		ArrayList<Vehicle> Garage = new ArrayList<Vehicle>();
 		Garage.add(new Twingo("Home", 180000, 68, "Poor"));
 		Garage.add(new RX7_FD("Home", 100000, 125, 270));
 		for(Vehicle v: Garage) {
 			System.out.println(v);
 		}
-	}
-	private void drive() {
+		System.out.println("Hello, and welcome to your garage! \n "
+				+ "What would you like to do today (enter lowercase word) \n"
+				+ "inspect \n"
+				+ "drive \n"
+				+ "repair");
+		Boolean tempGoodbye = false;
 		
 	}
 }
